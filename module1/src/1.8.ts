5ad998c5fc78de0689d5884364c13275d9113335
hamimDev <mdhamimhowladerasif@gmail.com>
2024-05-01 10:07:58 +0600

    Ts destructuring

--- module1/src/1.8.ts
+++ module1/src/1.8.ts
@@ -1,1 +1,33 @@
-// Ts destructuring
+{
+  // Ts destructuring
+
+  // Object destructuring
+  const user = {
+    id: 356,
+    Name: {
+      fastName: "Hamim",
+      middleName: "Howlader",
+      lastName: "Asif",
+    },
+    contactNo: "01714544654",
+    address: "Barishal",
+  };
+
+  const {
+    contactNo,
+    Name: { middleName },
+  } = user;
+
+  // Array destructuring
+  const myFriends = [
+    "Hamnim",
+    "Asif",
+    "Ontu",
+    "shihab",
+    "nabin",
+    "Ontor",
+    "Zihad",
+  ];
+
+  const [, , bestFriend, ...rest] = myFriends;
+}
