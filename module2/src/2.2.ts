0ef44dcb1f22c08e453e3edcd2ca7f1268f4f27d
hamimDev <mdhamimhowladerasif@gmail.com>
2024-05-03 09:34:18 +0600

    object, Array & function interface

---
+++ module2/src/2.2.ts
@@ -0,0 +1,44 @@
+{
+  // object interface
+  type User1 = {
+    name: string;
+    age: number;
+  };
+
+  interface User2 {
+    name: string;
+    age: number;
+  }
+
+  type UserRoll = User1 & {
+    roll: number;
+  };
+
+  interface UserRoll2 extends User1 {
+    roll: number;
+  }
+
+  const user: UserRoll2 = {
+    name: "Hamim",
+    age: 21,
+    roll: 54,
+  };
+
+  // Array interface
+
+  type Rolls1 = number[];
+  interface Rolls2 {
+    [index: number]: number;
+  }
+  const rollNumbers1: Rolls2 = [1, 2, 3, 4, 5];
+
+  // Function interface
+
+  type Add = (num1: number, num2: number) => number;
+
+  interface Add2 {
+    (num1: number, num2: number): number;
+  }
+
+  const add: Add2 = (num1, num2) => num1 + num2;
+}
