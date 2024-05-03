25b7444c92b9cc7e23cc12e0bae0735d9bbbaf6a
hamimDev <mdhamimhowladerasif@gmail.com>
2024-05-03 22:14:52 +0600

    load & fetch server Data

--- module2/src/2.8.ts
+++ module2/src/2.8.ts
@@ -1,8 +1,8 @@
 {
   // promise
 
-  const createPromise = () => {
-    return new Promise((resolve, reject) => {
+  const createPromise = (): Promise<string> => {
+    return new Promise<string>((resolve, reject) => {
       const data: string = "something";
       if (data) {
         resolve(data);
@@ -12,12 +12,33 @@
     });
   };
 
-  const showData = async () => {
-    const data = await createPromise();
-    console.log(data);
+  const showData = async (): Promise<string> => {
+    const data: string = await createPromise();
+    return data;
   };
 
   showData();
+
+  //   load server Data
+
+  //   const getData = async () => {
+  //     const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  //     const data = await res.json();
+  //     return data;
+  //   };
+
+  const getData = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+    return data;
+  };
+  getData()
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error("An error occurred:", error);
+    });
 
   //
 }
